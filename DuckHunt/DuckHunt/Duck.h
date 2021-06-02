d45d208da1ef886d13bde741aba46834d2c2b8a1
Elena Shavarska <e.shavarska@gmail.com>
2021-06-02 18:58:22 +0300

    Updated duck class

--- DuckHunt/DuckHunt/Duck.h
+++ DuckHunt/DuckHunt/Duck.h
@@ -8,9 +8,7 @@
 class Duck {
 public:
 	Duck(int w, int h, int x, int y);
-	//Duck(SDL_Renderer* renderer, int w, int h, int x, int y, const std::string& image_path);
 	~Duck();
-	//void draw(SDL_Renderer* renderer) const;
 	void eventHandler(SDL_Event& event);
 	bool die();
 	bool isAlive();
@@ -20,11 +18,10 @@ public:
 	SDL_Rect *getRect();
 
 private:
-	bool alive = 1;
+	bool alive;
 	int _w, _h;
 	int _x, _y;
 	SDL_Rect img;
-	/*SDL_Texture* _duck_texture = nullptr;*/
 };
 
 #endif
