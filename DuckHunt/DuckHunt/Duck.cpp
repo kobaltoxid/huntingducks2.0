2f6a8b13ae706632c4229995642ade1b8de1e6f4
Elena Shavarska <e.shavarska@gmail.com>
2021-06-02 19:14:51 +0300

    Implemented duck dying and falling to the ground

--- DuckHunt/DuckHunt/Duck.cpp
+++ DuckHunt/DuckHunt/Duck.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h> 
 #include <iostream>
 #include <string>
+#include<windows.h>
 
 Duck::Duck(int w, int h, int x, int y): 
 	_w(w), _h(h), _x(x), _y(y)
@@ -19,56 +20,64 @@ Duck::~Duck() {
 
 
 void Duck::move() {
-	srand(time(NULL));
-	int dir = rand() % 8 + 1;
-	int distance = rand() % 2 + 5;
-	switch (dir)
-	{
-	case 1:
-		if(_y - distance > 0)
-			_y -= distance;
-		break;
-	case 2:
-		if (_x + distance < 700 && _y - distance > 0) {
-			_x += distance;
-			_y -= distance;
+	if (alive) {
+		srand(time(NULL));
+		int dir = rand() % 8 + 1;
+		int distance = rand() % 2 + 5;
+		switch (dir)
+		{
+		case 1:
+			if (_y - distance > 0)
+				_y -= distance;
+			break;
+		case 2:
+			if (_x + distance < 700 && _y - distance > 0) {
+				_x += distance;
+				_y -= distance;
+			}
+			break;
+		case 3:
+			if (_x + distance < 700)
+				_x += distance;
+			break;
+		case 4:
+			if (_x + distance < 700 && _y + distance < 700) {
+				_x += distance;
+				_y += distance;
+			}
+			break;
+		case 5:
+			if (_y + distance < 700)
+				_y += distance;
+			break;
+		case 6:
+			if (_x - distance > 0 && _y + distance < 700) {
+				_x -= distance;
+				_y += distance;
+			}
+			break;
+		case 7:
+			if (_x - distance > 0)
+				_x -= distance;
+			break;
+		case 8:
+			if (_x - distance > 0 && _y - distance > 0) {
+				_x -= distance;
+				_y -= distance;
+			}
+			break;
+		default:
+			break;
 		}
-		break;
-	case 3:
-		if(_x + distance < 700)
-			_x += distance;
-		break;
-	case 4:
-		if (_x + distance < 700 && _y + distance < 700) {
-			_x += distance;
-			_y += distance;
-		}
-		break;
-	case 5:
-		if (_y + distance < 700)
-			_y += distance;
-		break;
-	case 6:
-		if (_x - distance > 0 && _y + distance < 700) {
-			_x -= distance;
-			_y += distance;
-		}		
-		break;
-	case 7:
-		if(_x - distance > 0)
-			_x -= distance;
-		break;
-	case 8:
-		if (_x - distance > 0 && _y - distance > 0) {
-			_x -= distance;
-			_y -= distance;
-		}
-		break;
-	default:
-		break;
-	}
 
-	img = { _x, _y, _w, _h };
+		img = { _x, _y, _w, _h };
+	}
+	else {
+		if (_y++ < 700) {
+			_y+=4;
+			img = { _x, _y, _w, _h };
+		}
+	}
 }
 
 int Duck::getX() {
