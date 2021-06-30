6353b14bcb203e066f01f87941eb6aa49fc47e64
Viktor Yankov <roterabe@yahoo.com>
2021-06-30 09:57:29 +0300

    chnages to accomodate emscripten

--- DuckHunt/DuckHunt/Main.cpp
+++ DuckHunt/DuckHunt/Main.cpp
@@ -1,15 +1,31 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 #include <iostream>
 #include <SDL.h>
 #include "exceptions/SDL_exception.h"
 #include "ui/Engine.h"
 
+void main_loop()
+{
+	Engine::getEngine()->Update();
+}
+
 int main(int argc, char* argv[]) {
 	try {
 		Engine::getEngine()->Init();
 	}
-	catch (SDL_exception &err) {
+	catch (SDL_exception& err) {
 		std::cout << err.what() << std::endl;
 	}
+
+#ifdef __EMSCRIPTEN__
+	emscripten_set_main_loop(main_loop(), 0, 1);
+#endif
+#ifndef __EMSCRIPTEN__
+	while (Engine::getEngine()->isRunning())
+		main_loop();
+#endif
 
 	while (Engine::getEngine()->isRunning())
 	{
@@ -18,6 +34,9 @@ int main(int argc, char* argv[]) {
 	}
 
 	Engine::getEngine()->Clean();
+#ifdef __EMSCRIPTEN__
+	emscripten_cancel_main_loop();
+#endif
 
 	return 0;
 }
